eef3735fdcb032eb2b0cfa72bf4e49372839ee6d
vidyah-cuelogic <vidya.hulwan@cuelogic.com>
2017-03-27 08:50:34 +0530

    updated

--- myproject1/myproject1/settings.py
+++ myproject1/myproject1/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db1',
-        'USER': 'myprojectuser',
+        'USER': 'myprojectuser_v',
         'PASSWORD': 'root123',
         'HOST': 'localhost',
         'PORT': '',
