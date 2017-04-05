6de68aa80de2c6b2910a622ed020a3c32c0b1fc2
vidyah-cuelogic <vidya.hulwan@cuelogic.com>
2017-04-05 21:02:19 +0530

    store input data into database and choose multiple colors by color picker

--- myproject1/app1/static/app1/js/color_picker.js
+++
@@ -1,6 +0,0 @@
- $(function() {
-     $('#color1').colorPicker({
-         onColorChange: function(id, newValue) { console.log("ID: " + id + " has been changed to " + newValue); }
-     });
-     $('#color2').colorPicker({ transparency: true });
- });
